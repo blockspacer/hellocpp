027072362d02f7fdbd5a68962d123bb3a0da7e49
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-17 16:46:00 +0100

    Simplify build script

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 echo "Compiling"
 g++ -o build/hello hello-repository/app/hello.cpp -static
-cp hello-repository/Procfile build/
 
 echo "Archiving binaries"
-zip -r build/hello.zip build/hello build/Procfile
+zip -r build/hello.zip build/hello hello-repository/Procfile
