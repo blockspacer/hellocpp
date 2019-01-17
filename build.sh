b339ea2a471022854e2e300cba90c209b50a5b59
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-17 16:42:46 +0100

    Configure correctly the dir of the archive

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ g++ -o build/hello hello-repository/app/hello.cpp -static
 cp hello-repository/Procfile build/
 
 echo "Archiving binaries"
-zip -r hello.zip build/hello build/Procfile
+zip -r build/hello.zip build/hello build/Procfile
