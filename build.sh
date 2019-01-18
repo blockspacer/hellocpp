432b5f7ccd7722f3fc2594c55efda9e5b1bc1923
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-18 16:52:18 +0100

    Fix boost dependency link

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compiling"
-g++ -o build/app repository/app/*.cpp -static -lboost_system  -lboost_thread-mt -std=c++11
+g++ -o build/app repository/app/*.cpp -static -lboost_system  -lboost_thread -std=c++11
 
 echo "Archiving binaries"
 zip -r build/app.zip build/app
