09d9455566cf7bf4bbcced2c33459bd691f7f011
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-21 10:15:10 +0100

    Add some debug logs

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ echo "Building the app"
 #g++ -o build/app repository/app/*.cpp -static -lboost_system  -lboost_thread -std=c++11
 cd build
 echo "Listing content of src dir:"
-ls -la repository/app
+ls -la repository
+
+echo "Listing content of build dir:"
+ls -la build
+
 echo "Step 1"
 conan install repository/app
 echo "Step 2"
