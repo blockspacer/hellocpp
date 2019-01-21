255a463bc4df860798262410eabea3435fc46847
Fouad Hamdi <1621862+fouadh@users.noreply.github.com>
2019-01-21 10:03:41 +0100

    Add conan dependencies manager

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Compiling"
-g++ -o build/app repository/app/*.cpp -static -lboost_system  -lboost_thread -std=c++11
+echo "Building the app"
+#g++ -o build/app repository/app/*.cpp -static -lboost_system  -lboost_thread -std=c++11
+cd build
+conan install repository/app
+cmake repository/app -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+cmake --build .
 
 echo "Archiving binaries"
 zip -r build/app.zip build/app
